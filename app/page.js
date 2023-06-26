2a60e3f9fc6732fda4c4d14ab568c8ab846814cc
Jared Araujo <jared.araujo@gmail.com>
2023-06-26 12:50:48 -0500

    modificación logo, cambio estructura

--- app/page.js
+++ app/page.js
@@ -29,15 +29,8 @@ export default function Home() {
       </div>
 
       <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        <h1>JEAR</h1>
+        </div>
 
       <div className={styles.grid}>
         <a
